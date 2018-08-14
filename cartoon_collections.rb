88bbfc85ae27b2f06f9f8119aab8ca12e1a39514
Jason Viger <jason.viger@gmail.com>
2018-08-14 09:58:05 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,24 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarves)# code an argument here
   # Your code here
+  y = 1
+  dwarves.each do |x|
+    puts "#{y}. #{x}"
+    y += 1
+  end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(planeteers)# code an argument here
   # Your code here
+  planeteers.collect { |x| x.capitalize + "!" } 
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(calls)# code an argument here
   # Your code here
+  long = false
+  if calls.any? {|x| x.size >= 4} 
+    long = true
+  end
+  long
 end
 
 def find_the_cheese# code an argument here
